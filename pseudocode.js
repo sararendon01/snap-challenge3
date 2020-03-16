7a1253a0c79cfc4e39fdd1a43a001fa0adffd6ec
srendon <srendon4@cnm.edu>
2020-03-16 09:32:05 -0600

    Algorithm

--- pseudocode.js
+++ pseudocode.js
@@ -4,4 +4,16 @@ A number is equal to x
 to convert kilogram(kg) to grams: kg/4000
 Therefore, x(g)=x(kg)/4000.
  */
+var number = 1;
+function uniquePositiveFactors(number1){
+	sum = 0;
+	for (i=1;i<number1;i++){
+		if(number1%i==0){
+			console.log(i);
+			sum += i;
+		}
+	}
+	console.log(number1 + sum)
+}
+let result = uniquePositiveFactors(40);
 
